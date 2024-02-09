c46dd037778b6dd7e629403f52b534ee254568d6
EngJao89 <eng_joao@outlook.com>
2024-02-09 16:50:41 -0300

    feat(ps08-orders): implements screen of orders

---
+++ src/pages/app/orders/orders.tsx
@@ -0,0 +1,49 @@
+import { Helmet } from 'react-helmet-async'
+
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+
+
+export function Orders() {
+  return (
+    <>
+      <Helmet title="Pedidos" />
+
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+        <div className="space-y-2.5">
+          <OrderTableFilters />
+
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {result &&
+                  result.orders.map((order) => {
+                    return <OrderTableRow key={order.orderId} order={order} />
+                  })}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
