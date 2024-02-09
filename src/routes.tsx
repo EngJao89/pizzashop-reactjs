f3760f9dd5e4bf268f50a3f53f5f61143292dc5f
EngJao89 <eng_joao@outlook.com>
2024-02-09 10:46:36 -0300

    feat(ps05-sonner): implements sonner and field of forms

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,9 +4,7 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { NotFound } from './pages/404'
 import { Dashboard } from './pages/app/dashboard/dashboard'
-import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
-import { SignUp } from './pages/auth/sign-up'
 import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
@@ -19,10 +17,6 @@ export const router = createBrowserRouter([
         path: '/',
         element: <Dashboard />,
       },
-      {
-        path: '/orders',
-        element: <Orders />,
-      },
     ],
   },
   {
@@ -32,10 +26,6 @@ export const router = createBrowserRouter([
       {
         path: '/sign-in',
         element: <SignIn />,
-      },
-      {
-        path: '/sign-up',
-        element: <SignUp />,
       },
     ],
   },
